e28ae7f2de369efcd67006cfafe40a97b43fe1d8
Mack Joshi <makarand13@gmail.com>
2017-12-06 23:21:28 -0500

    Cleanup

--- franbusiness.sh
+++ franbusiness.sh
@@ -5,6 +5,14 @@
 #echo "Filename with listing of brand URLs?"
 #read filename
 
+for i in `cat urls.txt`
+  do
+  	curl -v -s http://www.franchisebusiness.com.au/brands/$i#taball > $i.html
+  	grep "<dd>" -A 3 $i.html | grep -v "<dd>" | grep -v "</dd>" | sed -e 's/^[ \t]*//' | sed 's/<br\/>//g' | grep -v "\--" | sed '/^\s*$/d' > $i.txt
+    tr -d $'\r' < $i.txt > $i.out
+  done
+
+
 #for url in `cat bricks-4-kidz.html`
 #	do
 #		sitename=`echo $url | cut -f1 -d"#" | cut -f5 -d"/"`
@@ -13,7 +21,7 @@
 #		cat $sitename.html | grep "<h4>" | head -n -3 | cut -f2 -d">" | cut -f1 -d"<"
 #done
 
-grep "<dd>" -A 3 bricks-4-kidz.html | grep -v "<dd>" | grep -v "</dd>" | sed -e 's/^[ \t]*//' | sed 's/<br\/>//g' | grep -v "\--" | sed '/^\s*$/d' > abc
+#grep "<dd>" -A 3 bricks-4-kidz.html | grep -v "<dd>" | grep -v "</dd>" | sed -e 's/^[ \t]*//' | sed 's/<br\/>//g' | grep -v "\--" | sed '/^\s*$/d' > abc
 
 
 #From Notes
